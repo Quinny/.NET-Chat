efdc06cd40060dbdfb534f9252996fa2308beae8
Quinn Perfetto <thequinnftw@gmail.com>
2015-06-20 00:36:34 -0400

    UI updated a bit

--- Web/chat.js
+++ Web/chat.js
@@ -1,14 +1,20 @@
 var _ws;
 
-function sendMessage() {
-    _ws.send($("#message").val());
-}
-
-function registerName() {
-    _ws.send("//registerName:" + $("#username").val());
-}
-
 $("document").ready(function() {
+    $(".sendMessage").submit(function() {
+        _ws.send($("#message").val());
+        $("#message").val("");
+        return false;
+    });
+
+    $(".registerName").submit(function() {
+        _ws.send("//registerName:" + $("#username").val());
+        $(".registerName").hide();
+        $(".sendMessage").removeClass("hidden");
+        $("#message").focus();
+        return false;
+    });
+
     _ws = new WebSocket("ws://localhost:8181");
     _ws.onopen = function() {
     };
